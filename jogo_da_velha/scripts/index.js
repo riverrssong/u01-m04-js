a09bab532ec8df3d1c1b72bae28998746d9ea78d
Ruth Rios <ruthprios@outlook.com>
2022-10-21 22:05:01 -0300

    .

--- jogo_da_velha/scripts/index.js
+++ jogo_da_velha/scripts/index.js
@@ -1,3 +1,26 @@
+let gamer = "X";
+
+function winner () {
+    let elements = document.getElementById("tabuleiro").childElementCounthildren;
+    if(elements [0].innerText == elements[1].innerText &&
+    elements [1].innerText == elements[2].innerText &&
+    elements [0].innerText.length !== 0
+
+    ) {
+        elements[0].style="background-color: yellow;";
+        elements[1].style="background-color: yellow;";
+        elements[2].style="background-color: yellow;";
+
+    }
+}
 function mark(element) {
-element.innerText = "X";
+    if (element.innerText.length === 0) {
+        element.innerText = gamer;
+        winner()
+        if (gamer === "X") {
+            gamer = "O";
+        } else {
+            gamer = "X";
+        }
+    }
 }
